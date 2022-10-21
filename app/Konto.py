be41ac530a1c78b3bd3d601bfb94e175951990c6
Oskar Kuśmierek <oskar.kusmierekk@gmail.com>
2022-10-21 15:31:41 +0200

    pesel z testami

--- app/Konto.py
+++ app/Konto.py
@@ -1,6 +1,11 @@
 class Konto:
-    def __init__(self, imie, nazwisko,pesel):
+    def __init__(self, imie, nazwisko,pesel,rabat):
         self.imie = imie
         self.nazwisko = nazwisko
-        self.pesel = pesel
+        if(len(pesel)!=11):
+            self.pesel="Niepoprawny pesel"
+        else:
+            self.pesel = peselgiu
+        
         self.saldo = 0
+        self.rabat = rabat
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -5,9 +5,19 @@ from ..Konto import Konto
 class TestCreateBankAccount(unittest.TestCase):
 
     def test_tworzenie_konta(self):
-        pierwsze_konto = Konto("Dariusz", "Januszewski")
+        pierwsze_konto = Konto("Dariusz", "Januszewski","80090298994")
         self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
-        self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
+        self.assertEqual(pierwsze_konto.pesel, 0, "Saldo nie jest zerowe!")
+        self.assertEqual(pierwsze_konto.pesel,"80090298994","Pesel nie został zapisany")
+        self.assertEqual(len(pierwsze_konto.pesel),11,"Niepoprawny")
+    def test_tworzenie_konta(self):
+        pierwsze_konto = Konto("Kacper", "Kacperczyk","800902984")
+        self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
+        self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
+        self.assertEqual(pierwsze_konto.pesel, 0, "Saldo nie jest zerowe!")
+        self.assertEqual(pierwsze_konto.pesel,"80090298994","Pesel nie został zapisany")
+        self.assertEqual(len(pierwsze_konto.pesel),,"Niepoprawny")
+
 
     #tutaj proszę dodawać nowe testy
