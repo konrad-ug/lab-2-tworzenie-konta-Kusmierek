9fc864361123920e1b92bdc8c55aec13cae2a937
Kusmierek1 <oskar.kusmierekk@gmail.com>
2022-10-26 16:46:18 +0200

    feature 4 z testami

--- app/Konto.py
+++ app/Konto.py
@@ -1,11 +1,15 @@
 class Konto:
-    def __init__(self, imie, nazwisko,pesel):
+    def __init__(self, imie, nazwisko,pesel,promo=None):
         self.imie = imie
         self.nazwisko = nazwisko
         self.saldo = 0
         self.PeselValidation(pesel)
+        self.PromoChecker(promo)
     def PeselValidation(self,pesel):
         if(len(pesel)!=11):
             self.pesel = "Niepoprawny pesel"
         else:
             self.pesel=pesel
+    def PromoChecker(self,promo):
+        if(promo != None and promo.startswith('PROM_') and len(promo)==8):
+            self.saldo += 50
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -8,15 +8,23 @@ class TestCreateBankAccount(unittest.TestCase):
         pierwsze_konto = Konto("Dariusz", "Januszewski","80090298994")
         self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
-        self.assertEqual(pierwsze_konto.pesel, 0, "Saldo nie jest zerowe!")
+        self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
         self.assertEqual(pierwsze_konto.pesel,"80090298994","Pesel nie został zapisany")
 
-    def test_tworzenie_konta(self):
+        
+        
+    def test_niepoprawny_pesel(self):
         drugie_konto = Konto("Kacper", "Kacperczyk","800902984")
         self.assertEqual(drugie_konto.imie, "Kacper", "Imie nie zostało zapisane!")
         self.assertEqual(drugie_konto.nazwisko, "Kacperczyk", "Nazwisko nie zostało zapisane!")
         self.assertEqual(drugie_konto.saldo, 0, "Saldo nie jest zerowe!")
         self.assertEqual(drugie_konto.pesel,"Niepoprawny pesel","Pesel nie został zapisany")
 
-
+    def test_bonus(self):
+        trzecie_konto = Konto("Marcin", "Markowski","59092593383","PROM_ABC")
+        self.assertEqual(trzecie_konto.imie, "Marcin", "Imie nie zostało zapisane!")
+        self.assertEqual(trzecie_konto.nazwisko, "Markowski", "Nazwisko nie zostało zapisane!")
+        self.assertEqual(trzecie_konto.saldo, 50, "Saldo się nie zgadza!")
+        self.assertEqual(trzecie_konto.pesel,"59092593383","Pesel nie został zapisany")
+    
     #tutaj proszę dodawać nowe testy
