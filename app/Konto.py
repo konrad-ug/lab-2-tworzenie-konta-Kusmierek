d9a2e073e3f678b75a835393b05141d895867b58
Kusmierek1 <oskar.kusmierekk@gmail.com>
2022-10-26 11:31:46 +0200

    Pesel i testy, poprawa

--- app/Konto.py
+++ app/Konto.py
@@ -1,11 +1,11 @@
 class Konto:
-    def __init__(self, imie, nazwisko,pesel,rabat):
+    def __init__(self, imie, nazwisko,pesel):
         self.imie = imie
         self.nazwisko = nazwisko
-        if(len(pesel)!=11):
-            self.pesel="Niepoprawny pesel"
-        else:
-            self.pesel = peselgiu
-        
         self.saldo = 0
-        self.rabat = rabat
+        self.PeselValidation(pesel)
+    def PeselValidation(self,pesel):
+        if(len(pesel)!=11):
+            self.pesel = "Niepoprawny pesel"
+        else:
+            self.pesel=pesel
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -10,14 +10,13 @@ class TestCreateBankAccount(unittest.TestCase):
         self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.pesel, 0, "Saldo nie jest zerowe!")
         self.assertEqual(pierwsze_konto.pesel,"80090298994","Pesel nie został zapisany")
-        self.assertEqual(len(pierwsze_konto.pesel),11,"Niepoprawny")
+
     def test_tworzenie_konta(self):
-        pierwsze_konto = Konto("Kacper", "Kacperczyk","800902984")
-        self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
-        self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
-        self.assertEqual(pierwsze_konto.pesel, 0, "Saldo nie jest zerowe!")
-        self.assertEqual(pierwsze_konto.pesel,"80090298994","Pesel nie został zapisany")
-        self.assertEqual(len(pierwsze_konto.pesel),,"Niepoprawny")
+        drugie_konto = Konto("Kacper", "Kacperczyk","800902984")
+        self.assertEqual(drugie_konto.imie, "Kacper", "Imie nie zostało zapisane!")
+        self.assertEqual(drugie_konto.nazwisko, "Kacperczyk", "Nazwisko nie zostało zapisane!")
+        self.assertEqual(drugie_konto.saldo, 0, "Saldo nie jest zerowe!")
+        self.assertEqual(drugie_konto.pesel,"Niepoprawny pesel","Pesel nie został zapisany")
 
 
     #tutaj proszę dodawać nowe testy
